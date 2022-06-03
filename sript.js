331352497a14be0cc7fe0cc1db0315a50a24ceeb
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2022-06-03 12:53:29 +0300

    add:header_media

--- sript.js
+++ sript.js
@@ -16,3 +16,24 @@ document.querySelectorAll('.tabs-wrapper').forEach((e) => {
         }
     }
 });
+
+
+/* Прикрепить меню хедера */
+let scrollpos = window.scrollY
+
+const header = document.querySelector("header")
+const scrollChange = 1
+
+const add_class_on_scroll = () => header.classList.add("header_fixed")
+const remove_class_on_scroll = () => header.classList.remove("header_fixed")
+
+window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= scrollChange) {
+        add_class_on_scroll()
+    } else {
+        remove_class_on_scroll()
+    }
+
+})
